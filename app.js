fcdc58957fbf54441dd7dfa8ea7dd0d859a99ac7
Sarah Dickinson <sarahadickinson@gmail.com>
2021-10-10 19:06:15 -0400

    modularize code

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2);
 
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
+const [name, github] = profileDataArgs;
 
-//     console.log('================');
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-
-const generatePage = () => 'Name: Jane, Github: janehub';
-
-console.log(generatePage());
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
